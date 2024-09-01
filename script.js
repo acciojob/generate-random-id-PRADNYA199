e3144ffdedea253a4b1f59e5d3d13ab21072ff1e
Pradnya Chandrakant Bhosale <67332759+PRADNYA199@users.noreply.github.com>
2024-09-01 05:05:47 -0700

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 function makeid(l) {
   // write your code here
+	let res=" ";
+	let char_list="ABCDEFGHIJKLMNOPQRSTUVWXzabcdefghijklmnopqrstuvwxyz0123456789";
+	for(var i=0;i<l;i++)
+		{
+			res+=char_list.charAt(
+		   Math.floor(Math.random()*char_list.length
+					 )
+				)
+		}
+	return res;
 }
 
 // Do not change the code below.
-const l = prompt("Enter a number.");
-alert(makeid(l));
+// const l = prompt("Enter a number.");
+// alert(makeid(l));
